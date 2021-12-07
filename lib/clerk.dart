3a65f23dd16fc02d076a3258df977047bcf44052
Santo Shakil <shakil.mezbah@gmail.com>
2021-12-07 20:00:42 +0600

    fixes

--- lib/clerk.dart
+++ lib/clerk.dart
@@ -4,38 +4,26 @@ import 'dart:io' show HttpServer, HttpRequest, WebSocket, WebSocketTransformer;
 
 Future<void> webSocket() async => HttpServer.bind('0.0.0.0', 8000).then(
       (server) => server.listen(
-        (HttpRequest request) {
-          print('[+]WebSocket listening at -- ws://0.0.0.0:8000/');
-          WebSocketTransformer.upgrade(request).then(
-            (WebSocket ws) {
-              ws.listen(
-                (data) {
-                  print(
-                      '${request.connectionInfo?.remoteAddress} - ${json.decode(data)}');
-                  Timer(
-                    Duration(seconds: 3),
-                    () {
-                      if (ws.readyState == WebSocket.open) {
-                        ws.add(
-                          json.encode(
-                            {
-                              'data': 'Server: ${DateTime.now().toString()}',
-                            },
-                          ),
-                        );
-                      }
-                    },
-                  );
-                },
-                onError: (err) => print('[!]Error -- ${err.toString()}'),
-                onDone: () => print('[+]Done :)'),
-                cancelOnError: true,
-              );
-            },
+        (request) => WebSocketTransformer.upgrade(request).then(
+          (ws) => ws.listen(
+            (data) => _task(request, ws, data),
             onError: (err) => print('[!]Error -- ${err.toString()}'),
-          );
-        },
+            onDone: () => print('[+]Done :)'),
+            cancelOnError: true,
+          ),
+          onError: (err) => print('[!]Error -- ${err.toString()}'),
+        ),
         onError: (err) => print('[!]Error -- ${err.toString()}'),
       ),
       onError: (err) => print('[!]Error -- ${err.toString()}'),
     );
+
+void _task(HttpRequest request, WebSocket ws, data) {
+  print('${request.connectionInfo?.remoteAddress} - ${json.decode(data)}');
+  Timer(
+    Duration(seconds: 3),
+    () => ws.readyState == WebSocket.open
+        ? ws.add(json.encode({'data': 'Server: ${DateTime.now().toString()}'}))
+        : print('[!]Error -- WebSocket is not open'),
+  );
+}
