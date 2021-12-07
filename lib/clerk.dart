6d523272af3a0b1a1008eedc43df3cb7ce80d47a
Santo Shakil <shakil.mezbah@gmail.com>
2021-12-07 19:36:02 +0600

    Fixes

--- lib/clerk.dart
+++ lib/clerk.dart
@@ -2,25 +2,40 @@ import 'dart:io' show HttpServer, HttpRequest, WebSocket, WebSocketTransformer;
 import 'dart:convert' show json;
 import 'dart:async' show Timer;
 
-void webSocket() => HttpServer.bind('0.0.0.0', 8000).then((HttpServer server) {
-      print('[+]WebSocket listening at -- ws://0.0.0.0:8000/');
-      server.listen((HttpRequest request) {
-        WebSocketTransformer.upgrade(request).then((WebSocket ws) {
-          ws.listen(
-            (data) {
-              print(
-                  '${request.connectionInfo?.remoteAddress} - ${json.decode(data)}');
-              Timer(Duration(seconds: 1), () {
-                if (ws.readyState == WebSocket.open) {
-                  ws.add(json.encode(
-                      {'data': 'Server: ${DateTime.now().toString()}'}));
-                }
-              });
+Future<void> webSocket() async => HttpServer.bind('0.0.0.0', 8000).then(
+      (server) => server.listen(
+        (HttpRequest request) {
+          print('[+]WebSocket listening at -- ws://0.0.0.0:8000/');
+          WebSocketTransformer.upgrade(request).then(
+            (WebSocket ws) {
+              ws.listen(
+                (data) {
+                  print(
+                      '${request.connectionInfo?.remoteAddress} - ${json.decode(data)}');
+                  Timer(
+                    Duration(seconds: 3),
+                    () {
+                      if (ws.readyState == WebSocket.open) {
+                        ws.add(
+                          json.encode(
+                            {
+                              'data': 'Server: ${DateTime.now().toString()}',
+                            },
+                          ),
+                        );
+                      }
+                    },
+                  );
+                },
+                onError: (err) => print('[!]Error -- ${err.toString()}'),
+                onDone: () => print('[+]Done :)'),
+                cancelOnError: true,
+              );
             },
-            onDone: () => print('[+]Done :)'),
             onError: (err) => print('[!]Error -- ${err.toString()}'),
-            cancelOnError: true,
           );
-        }, onError: (err) => print('[!]Error -- ${err.toString()}'));
-      }, onError: (err) => print('[!]Error -- ${err.toString()}'));
-    }, onError: (err) => print('[!]Error -- ${err.toString()}'));
+        },
+        onError: (err) => print('[!]Error -- ${err.toString()}'),
+      ),
+      onError: (err) => print('[!]Error -- ${err.toString()}'),
+    );
